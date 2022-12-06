dc7e5b56b3427682db537d207f3fccf3b34a50d7
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-06 07:37:51 +0100

    Solve Day 6

--- Day6/Sources/Day6/Day6.swift
+++ Day6/Sources/Day6/Day6.swift
@@ -1,1 +1,30 @@
+/// Finds the packet header marker in a datastream buffer
+///
+/// The header is a String of 4 non-repeating characters
+/// - Parameter input: A String representing a datastream buffer
+/// - Returns: The location of the last character of the marker. 1-indexed
+func findBeginningOfPacket(_ input: String) -> Int {
+    return input.findEndOfNonRepeatingSequence(of: 4)
+}
 
+/// Finds the message start marker of a datastream buffer
+///
+/// The message start marker is a String of 14 non-repeating characters
+/// - Parameter input: A String representing a message
+/// - Returns: The location of the last character of the marker. 1-indexed
+func findBeginningOfMessage(_ input: String) -> Int {
+    return input.findEndOfNonRepeatingSequence(of: 14)
+}
+
+private extension String {
+    func findEndOfNonRepeatingSequence(of length: Int) -> Int {
+
+        for index in 0... {
+            if Set(dropFirst(index).prefix(length)).count == length {
+                return index + length
+            }
+        }
+
+        fatalError("No non-repeating sequence of length \(length) was found")
+    }
+}
--- Day6/Tests/Day6Tests/Day6Tests.swift
+++ Day6/Tests/Day6Tests/Day6Tests.swift
@@ -2,11 +2,41 @@ import XCTest
 @testable import Day6
 
 final class Day6Tests: XCTestCase {
+    func testPart1Example() {
+        let cases = [
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)
+        ]
+
+        for c in cases {
+            XCTAssertEqual(findBeginningOfPacket(c.0), c.1)
+        }
+    }
+
+    func testPart2Example() {
+        let cases = [
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)
+        ]
+
+        for c in cases {
+            XCTAssertEqual(findBeginningOfMessage(c.0), c.1)
+        }
+    }
+
     func testDay6Part1() {
-        print("Day 6 Part 1:")
+        let answer = findBeginningOfPacket(input)
+        print("Day 6 Part 1:", answer)
     }
 
     func testDay6Part2() {
-        print("Day 6 Part 2:")
+        let answer = findBeginningOfMessage(input)
+        print("Day 6 Part 2:", answer)
     }
 }
