2b0ed2011a4a843bcbf30b323ad5195928b07cd3
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-10 16:13:49 +0100

    Solve Day 10

--- Day10/Sources/Day10/Day10.swift
+++ Day10/Sources/Day10/Day10.swift
@@ -1,1 +1,68 @@
+struct CPU {
 
+    var x = 1
+    var cycle = 0
+    var instructions: [Instruction]
+    var pendingInstruction: Instruction?
+    var signals: [Int] = []
+
+    var crt = ""
+
+    init(_ input: String) {
+        instructions = input.split(separator: "\n").map(Instruction.init)
+    }
+
+    mutating func runCycles() {
+        while !instructions.isEmpty {
+            let hIndex = (cycle % 40)
+
+            cycle += 1
+
+            if [20, 60, 100, 140, 180, 220].contains(cycle) {
+                signals.append(cycle * x)
+            }
+
+            if x - 1 == hIndex || x == hIndex || x + 1 == hIndex {
+                crt.append("#")
+            } else {
+                crt.append(".")
+            }
+            if cycle.isMultiple(of: 40) {
+                crt.append("\n")
+            }
+
+            if case let .addx(v) = pendingInstruction {
+                x += v
+                pendingInstruction = nil
+            } else {
+                let instruction = instructions.removeFirst()
+                switch instruction {
+                case .noop:
+                    break
+                case .addx:
+                    pendingInstruction = instruction
+                }
+            }
+        }
+        crt.removeLast()
+    }
+
+    var signalStrength: Int {
+        signals.reduce(0, +)
+    }
+}
+
+enum Instruction {
+    case addx(Int)
+    case noop
+
+    init(_ s: Substring) {
+        if s.wholeMatch(of: #/noop/#) != nil {
+            self = .noop
+        } else if let m = s.wholeMatch(of: #/addx (-?\d+)/#) {
+            self = .addx(Int(m.1)!)
+        } else {
+            fatalError("Unknown instruction \(s)")
+        }
+    }
+}
--- Day10/Tests/Day10Tests/Day10Tests.swift
+++ Day10/Tests/Day10Tests/Day10Tests.swift
@@ -2,11 +2,189 @@ import XCTest
 @testable import Day10
 
 final class Day10Tests: XCTestCase {
+    func testPart1Example() {
+        var cpu = CPU(testInput)
+        cpu.runCycles()
+
+        XCTAssertEqual(cpu.signalStrength, 13140)
+    }
+
+    func testPart2Example() {
+        var cpu = CPU(testInput)
+        cpu.runCycles()
+
+        let expected = """
+        ##..##..##..##..##..##..##..##..##..##..
+        ###...###...###...###...###...###...###.
+        ####....####....####....####....####....
+        #####.....#####.....#####.....#####.....
+        ######......######......######......####
+        #######.......#######.......#######.....
+        """
+
+        XCTAssertEqual(cpu.crt, expected)
+    }
+
     func testDay10Part1() {
-        print("Day 10 Part 1:")
+        var cpu = CPU(input)
+        cpu.runCycles()
+
+        print("Day 10 Part 1:", cpu.signalStrength)
     }
 
     func testDay10Part2() {
-        print("Day 10 Part 2:")
+        var cpu = CPU(input)
+        cpu.runCycles()
+
+        print("Day 10 Part 2:", cpu.crt, separator: "\n")
     }
 }
+
+private let testInput = """
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
