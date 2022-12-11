6f0de9689d4798a721d9083a214ca28e0f615b48
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-11 16:53:38 +0100

    Solve Day 11

--- Day11/Sources/Day11/Day11.swift
+++ Day11/Sources/Day11/Day11.swift
@@ -1,1 +1,81 @@
+private typealias ItemValue = UInt
+private typealias MonkeyIndex = Array<Monkey>.Index
 
+enum Mode {
+    case part1
+    case part2
+}
+
+func playMonkeyGame(input: String, rounds: Int, mode: Mode) -> Int {
+    var monkeys = input.split(separator: "\n\n").map(Monkey.init)
+    let commonTestNumber = monkeys.reduce(1, { $0 * $1.testValue })
+
+    for _ in 0..<rounds {
+        var index = monkeys.startIndex
+
+        while index < monkeys.endIndex {
+            var monkey = monkeys[index]
+            while !monkey.items.isEmpty {
+                monkey.itemsHeld += 1
+
+                var item = monkey.items.removeFirst()
+                item = monkey.operation(item)
+                if mode == .part1 {
+                    item /= 3
+                } else {
+                    item %= commonTestNumber
+                }
+                let target = item.isMultiple(of: monkey.testValue)
+                    ? monkey.monkeyIndices.0
+                    : monkey.monkeyIndices.1
+                monkeys[target].items.append(item)
+            }
+            monkeys[index] = monkey
+
+            index = monkeys.index(after: index)
+        }
+    }
+
+    let topMonkeys = monkeys.sorted { $0.itemsHeld > $1.itemsHeld }
+    return topMonkeys[0].itemsHeld * topMonkeys[1].itemsHeld
+}
+
+private struct Monkey {
+    var items: [ItemValue]
+    var itemsHeld = 0
+    let operation: (ItemValue) -> ItemValue
+    let testValue: ItemValue
+    let monkeyIndices: (MonkeyIndex, MonkeyIndex)
+
+    init(_ input: Substring) {
+        let m = input.firstMatch(of: #/
+        Monkey\ \d:\
+        \ \ Starting\ items:\ (.*)\
+        \ \ Operation:\ (.*)\
+        \ \ Test:\ divisible\ by\ (\d+)\
+        \ \ \ \ If\ true:\ throw\ to\ monkey\ (\d)\
+        \ \ \ \ If\ false:\ throw\ to\ monkey\ (\d)
+        /#)!
+
+        items = m.1.split(separator: ", ").compactMap { ItemValue($0) }
+        operation = parseOperation(for: m.2)
+        testValue = ItemValue(m.3)!
+        monkeyIndices = (MonkeyIndex(m.4)!, MonkeyIndex(m.5)!)
+    }
+}
+
+private func parseOperation(for input: Substring) -> (ItemValue) -> ItemValue {
+    let m = input.wholeMatch(of: #/new = old (\+|\*) (old|\d+)/#)!
+    switch (m.1, m.2) {
+    case ("*", "old"):
+        return { $0 * $0 }
+    case ("+", "old"):
+        return { $0 + $0 }
+    case ("*", let arg):
+        return { $0 * ItemValue(arg)! }
+    case ("+", let arg):
+        return { $0 + ItemValue(arg)! }
+    default:
+        fatalError("Unexpected pattern \(input)")
+    }
+}
--- Day11/Tests/Day11Tests/Day11Tests.swift
+++ Day11/Tests/Day11Tests/Day11Tests.swift
@@ -2,11 +2,85 @@ import XCTest
 @testable import Day11
 
 final class Day11Tests: XCTestCase {
+    func testPart1Example() {
+        let input = """
+        Monkey 0:
+          Starting items: 79, 98
+          Operation: new = old * 19
+          Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+
+        Monkey 1:
+          Starting items: 54, 65, 75, 74
+          Operation: new = old + 6
+          Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+
+        Monkey 2:
+          Starting items: 79, 60, 97
+          Operation: new = old * old
+          Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+
+        Monkey 3:
+          Starting items: 74
+          Operation: new = old + 3
+          Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
+        """
+
+        let answer = playMonkeyGame(input: input, rounds: 20, mode: .part1)
+
+        XCTAssertEqual(answer, 10605)
+    }
+
     func testDay11Part1() {
-        print("Day 11 Part 1:")
+        let answer = playMonkeyGame(input: input, rounds: 20, mode: .part1)
+        print("Day 11 Part 1:", answer)
+    }
+
+    func testPart2Example() {
+        let input = """
+        Monkey 0:
+          Starting items: 79, 98
+          Operation: new = old * 19
+          Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+
+        Monkey 1:
+          Starting items: 54, 65, 75, 74
+          Operation: new = old + 6
+          Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+
+        Monkey 2:
+          Starting items: 79, 60, 97
+          Operation: new = old * old
+          Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+
+        Monkey 3:
+          Starting items: 74
+          Operation: new = old + 3
+          Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
+        """
+
+        let answer = playMonkeyGame(input: input, rounds: 10000, mode: .part2)
+
+        XCTAssertEqual(answer, 2713310158)
     }
 
     func testDay11Part2() {
-        print("Day 11 Part 2:")
+        let answer = playMonkeyGame(input: input, rounds: 10000, mode: .part2)
+        print("Day 11 Part 2:", answer)
     }
 }
