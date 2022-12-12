73bb9410605302a96cb4d5605fca07ebd6c95ed5
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-12 08:30:52 +0100

    Solve Day 12

--- Day12/Tests/Day12Tests/Day12Tests.swift
+++ Day12/Tests/Day12Tests/Day12Tests.swift
@@ -2,11 +2,41 @@ import XCTest
 @testable import Day12
 
 final class Day12Tests: XCTestCase {
+    func testPart1Example() {
+        let input = """
+        Sabqponm
+        abcryxxl
+        accszExk
+        acctuvwj
+        abdefghi
+        """
+
+        let answer = leastAmountOfStepsPart1(input)
+
+        XCTAssertEqual(answer, 31)
+    }
+
+    func testPart2Example() {
+        let input = """
+        Sabqponm
+        abcryxxl
+        accszExk
+        acctuvwj
+        abdefghi
+        """
+
+        let answer = leastAmountOfStepsPart2(input)
+
+        XCTAssertEqual(answer, 29)
+    }
+
     func testDay12Part1() {
-        print("Day 12 Part 1:")
+        let answer = leastAmountOfStepsPart1(input)
+        print("Day 12 Part 1:", answer)
     }
 
     func testDay12Part2() {
-        print("Day 12 Part 2:")
+        let answer = leastAmountOfStepsPart2(input)
+        print("Day 12 Part 2:", answer)
     }
 }
