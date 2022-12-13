d3fea65c081b1ab5f599f90f0e5a9a3c96ce47b8
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-13 07:57:17 +0100

    Solve Day 13

--- Day13/Sources/Day13/Day13.swift
+++ Day13/Sources/Day13/Day13.swift
@@ -1,1 +1,103 @@
+func scoreOfPairsInTheRightOrder(_ input: String) -> Int {
+    let pairs = input.split(separator: "\n\n").map {
+        let l1 = List($0.split(separator: "\n")[0])!
+        let l2 = List($0.split(separator: "\n")[1])!
 
+        return (l1, l2)
+    }
+
+    return zip(1..., pairs.map(<)).reduce(into: 0) {
+        let (index, isInRightOrder) = $1
+        if isInRightOrder { $0 += index }
+    }
+}
+
+func decoderKey(_ input: String) -> Int {
+    let dividerPacket1: List = [[2]]
+    let dividerPacket2: List = [[6]]
+
+    let elementsFromInput = input.split(separator: "\n").compactMap(List.init)
+
+    let allElements = (elementsFromInput + [dividerPacket1, dividerPacket2])
+        .sorted()
+
+    let i1 = allElements.firstIndex(of: dividerPacket1)!
+    let i2 = allElements.firstIndex(of: dividerPacket2)!
+    return (i1 + 1) * (i2 + 1)
+}
+
+public enum List: Equatable {
+    case list([List])
+    case literal(Int)
+
+    init?<S: StringProtocol>(_ input: S) where S.SubSequence == Substring {
+        var input = input[...]
+        guard let list = List(&input) else { return nil }
+        self = list
+        assert(input.isEmpty)
+    }
+
+    init?(_ input: inout Substring) {
+        if input.first == "[" {
+            input.removeFirst()
+            var nodes: [List] = []
+            while !input.isEmpty, let node = List(&input) {
+                nodes.append(node)
+            }
+            self = .list(nodes)
+        } else if input.first == "]" {
+            input.removeFirst()
+            if input.first == "," { input.removeFirst() }
+            return nil
+        } else if input.first?.isWholeNumber == true {
+            let number = input.prefix(while: \.isWholeNumber)
+            input.removeFirst(number.count)
+            if input.first == "," { input.removeFirst() }
+            self = .literal(Int(number)!)
+        } else {
+            fatalError("Unexpected input \(input)")
+        }
+    }
+}
+
+extension List: Comparable {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case let (.literal(l1), .literal(l2)):
+            return l1 < l2
+        case let (.list(l1), .list(l2)):
+            return l1 < l2
+        case let (.list(list), .literal):
+            return list < [rhs]
+        case let (.literal, .list(list)):
+            return [lhs] < list
+        }
+    }
+}
+
+extension Array: Comparable where Element == List {
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        for (l1, l2) in zip(lhs, rhs) {
+            if l1 < l2 {
+                return true
+            }
+            if l1 > l2 {
+                return false
+            }
+        }
+
+        return lhs.count < rhs.count
+    }
+}
+
+extension List: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: IntegerLiteralType) {
+        self = .literal(value)
+    }
+}
+
+extension List: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: List...) {
+        self = .list(Array(elements))
+    }
+}
--- Day13/Tests/Day13Tests/Day13Tests.swift
+++ Day13/Tests/Day13Tests/Day13Tests.swift
@@ -2,11 +2,75 @@ import XCTest
 @testable import Day13
 
 final class Day13Tests: XCTestCase {
+    func testPart1Example() {
+        let input = """
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
+        """
+
+        let answer = scoreOfPairsInTheRightOrder(input)
+        XCTAssertEqual(answer, 13)
+    }
+
+    func testPart2Example() {
+        let input = """
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
+        """
+
+        let answer = decoderKey(input)
+        XCTAssertEqual(answer, 140)
+    }
+
     func testDay13Part1() {
-        print("Day 13 Part 1:")
+        let answer = scoreOfPairsInTheRightOrder(input)
+        print("Day 13 Part 1:", answer)
     }
 
     func testDay13Part2() {
-        print("Day 13 Part 2:")
+        let answer = decoderKey(input)
+        print("Day 13 Part 2:", answer)
     }
 }
