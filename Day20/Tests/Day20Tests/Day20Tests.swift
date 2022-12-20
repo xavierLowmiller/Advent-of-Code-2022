2aa9ba829e4670bcbc46c80b2151b93f94244167
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-20 18:43:35 +0100

    Solve Day 20

--- Day20/Tests/Day20Tests/Day20Tests.swift
+++ Day20/Tests/Day20Tests/Day20Tests.swift
@@ -2,11 +2,54 @@ import XCTest
 @testable import Day20
 
 final class Day20Tests: XCTestCase {
+
+    func testPart1Example() {
+        let input = """
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4
+        """
+
+        var decryption = Decryption(input, isPart2: false)
+        decryption.mix()
+        let answer = decryption.score
+
+        XCTAssertEqual(answer, 3)
+    }
+
+    func testPart2Example() {
+        let input = """
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4
+        """
+
+        var decryption = Decryption(input, isPart2: true)
+        decryption.mix()
+        let answer = decryption.score
+
+        XCTAssertEqual(answer, 1623178306)
+    }
+
     func testDay20Part1() {
-        print("Day 20 Part 1:")
+        var decryption = Decryption(input, isPart2: false)
+        decryption.mix()
+
+        print("Day 20 Part 1:", decryption.score)
     }
 
     func testDay20Part2() {
-        print("Day 20 Part 2:")
+        var decryption = Decryption(input, isPart2: true)
+        decryption.mix()
+
+        print("Day 20 Part 2:", decryption.score)
     }
 }
