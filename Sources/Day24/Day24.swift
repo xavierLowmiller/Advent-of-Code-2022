05dfea168163879f7e2253d7bf7d72f0e6e892ba
Xaver Lohmüller <xaver.lohmueller@me.com>
2022-12-28 14:41:36 +0100

    Solve Day 24 Part 2

--- Sources/Day24/Day24.swift
+++ Sources/Day24/Day24.swift
@@ -9,6 +9,21 @@ struct Direction: OptionSet {
     static let allCases = [Self.up, .down, .left, .right]
 }
 
+struct Position: Hashable {
+    var x: Int
+    var y: Int
+
+    var neighbors: Set<Position> {
+        [
+            self,
+            Position(x: x + 1, y: y),
+            Position(x: x - 1, y: y),
+            Position(x: x, y: y + 1),
+            Position(x: x, y: y - 1),
+        ]
+    }
+}
+
 public func findWayThroughBlizzard(_ input: String) -> Int {
     let valley = BlizzardValley(input)
     let goal = valley.exit
@@ -19,57 +34,94 @@ public func findWayThroughBlizzard(_ input: String) -> Int {
         neighbors: \.neighbors
     )!
 
-//    print(path.map(\.description).joined(separator: "\n"))
-
     return path.count - 1
 }
 
-struct BlizzardValley {
-    let min = (x: 0, y: 0)
-    let max: (x: Int, y: Int)
-    let cycle: Int
-    let exit: (x: Int, y: Int)
+public func findWayThroughBlizzardAndBack(_ input: String) -> Int {
+    var valley = BlizzardValley(input)
+    let start = valley.initial
+    let goal = valley.exit
 
-    let initial = (x: 1, y: 0)
-    var position = (x: 1, y: 0)
-    private var index = 0
-    private var blizzards: [[Direction]]
+    let path1 = bfs(
+        start: valley,
+        goal: { $0.position == goal },
+        neighbors: \.neighbors
+    )!
+
+    valley.round += path1.count - 1
+    valley.position = goal
+    valley.initial = goal
+    valley.exit = start
+
+    let path2 = bfs(
+        start: valley,
+        goal: { $0.position == start },
+        neighbors: \.neighbors
+    )!
+
+    valley.round += path2.count - 1
+    valley.position = start
+    valley.initial = start
+    valley.exit = goal
+
+    let path3 = bfs(
+        start: valley,
+        goal: { $0.position == goal },
+        neighbors: \.neighbors
+    )!
+
+    valley.round += path3.count - 1
+
+    return valley.round
+}
+
+struct BlizzardValley {
+    let min = Position(x: 0, y: 0)
+    let max: Position
+    let rounds: Int
+    var exit: Position
+
+    var initial = Position(x: 1, y: 0)
+    var position = Position(x: 1, y: 0)
+    var round = 0
+    /// A map of the number of the round to the currently empty spots
+    private let freeSpaces: [Int: Set<Position>]
 
     init(_ input: String) {
         let lines = input.split(separator: "\n")
-        blizzards = Array(repeating: Array(repeating: [], count: lines[0].count - 1), count: lines.count - 1)
+        var blizzards: [Position: Direction] = [:]
         for (y, line) in lines.enumerated() {
             for (x, c) in line.enumerated() {
                 switch c {
                 case "#", ".":
                     break
                 case ">":
-                    blizzards[y][x].insert(.right)
+                    blizzards[Position(x: x, y: y)] = .right
                 case "<":
-                    blizzards[y][x].insert(.left)
+                    blizzards[Position(x: x, y: y)] = .left
                 case "^":
-                    blizzards[y][x].insert(.up)
+                    blizzards[Position(x: x, y: y)] = .up
                 case "v":
-                    blizzards[y][x].insert(.down)
+                    blizzards[Position(x: x, y: y)] = .down
 
                 default:
                     fatalError("Unexpected Character \(c)")
                 }
             }
         }
-        max = (blizzards[0].count, blizzards.count)
-        exit = (x: blizzards[0].count - 1, y: blizzards.count)
-        cycle = lcm(blizzards.count, blizzards[0].count)
-    }
 
-    mutating func step() {
-        let oldBlizzards = blizzards
-        blizzards = Array(repeating: Array(repeating: [], count: blizzards[0].count), count: blizzards.count)
-        for (y, line) in oldBlizzards.enumerated() {
-            for (x, bs) in line.enumerated() {
+        max = Position(x: lines[0].count - 1, y: lines.count - 1)
+        exit = Position(x: max.x - 1, y: max.y)
+        rounds = lcm(max.x + 1, max.y + 1)
+
+        var freeSpaces: [Int: Set<Position>] = [:]
+        for round in 0..<rounds {
+            let oldBlizzards = blizzards
+            blizzards = Dictionary(minimumCapacity: blizzards.count)
+            for (position, bs) in oldBlizzards {
                 for direction in Direction.allCases {
                     guard bs.contains(direction) else { continue }
-                    var position = (x: x, y: y)
+                    var position = position
                     switch direction {
                     case .up:
                         position.y -= 1
@@ -94,117 +146,51 @@ struct BlizzardValley {
                     default:
                         fatalError()
                     }
-                    blizzards[position.y][position.x].insert(direction)
+                    blizzards[position, default: []].insert(direction)
                 }
             }
+            freeSpaces[round] = emptySpots(in: blizzards, min: min, max: max)
+                .union([exit, initial])
         }
-        index = (index + 1) % cycle
+
+        assert(freeSpaces.count == rounds)
+        self.freeSpaces = freeSpaces
+    }
+
+    mutating func step() {
+        round += 1
     }
 
     var neighbors: [BlizzardValley] {
-        var nextValley = self
-        nextValley.step()
-        return nextValley.possibleSteps.map { p in
-            var valley = nextValley
-            valley.position = p
+        position.neighbors.intersection(freeSpaces[round % rounds]!).map {
+            var valley = self
+            valley.step()
+            valley.position = $0
             return valley
         }
     }
+}
 
-    private var possibleSteps: [(Int, Int)] {
-        var steps: [(Int, Int)] = Direction.allCases.compactMap {
-            var p = position
-            switch $0 {
-            case .up:
-                p.y -= 1
-            case .down:
-                p.y += 1
-            case .left:
-                p.x -= 1
-            case .right:
-                p.x += 1
-            default:
-                fatalError()
+private func emptySpots(in blizzards: [Position: Direction], min: Position, max: Position) -> Set<Position> {
+    var set = Set<Position>(minimumCapacity: max.x * max.y)
+    for x in (min.x + 1)..<max.x {
+        for y in (min.y + 1)..<max.y {
+            let p = Position(x: x, y: y)
+            if !blizzards.keys.contains(p) {
+                set.insert(p)
             }
-
-            if p == exit { return p }
-
-            guard min.x < p.x, p.x < max.x,
-                  min.y < p.y, p.y < max.y,
-                  blizzards[p.y][p.x] == []
-            else { return nil }
-            return p
         }
-
-        // Do nothing
-        if blizzards[position.y][position.x] == [] {
-            steps.append(position)
-        }
-
-        return steps
     }
-
-    var description: String {
-        var description = ""
-        for y in min.y...max.y {
-            for x in min.x...max.x {
-                let p = (x: x, y: y)
-                if p == position {
-                    description.append("E")
-                } else if p == initial || p == exit {
-                    description.append(".")
-                } else if p.x == min.x || p.x == max.x || p.y == min.y || p.y == max.y {
-                    description.append("#")
-                } else {
-                    switch blizzards[y][x] {
-                    case []:
-                        description.append(".")
-                    case .right:
-                        description.append(">")
-                    case .left:
-                        description.append("<")
-                    case .up:
-                        description.append("^")
-                    case .down:
-                        description.append("v")
-                    case let b:
-                        description.append("\(b.rawValue.nonzeroBitCount)")
-                    }
-                }
-            }
-            description.append("\n")
-        }
-        return description
-    }
+    return set
 }
 
 extension BlizzardValley: Hashable {
     static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.position == rhs.position && lhs.index == rhs.index
+        lhs.position == rhs.position && lhs.round == rhs.round
     }
 
     func hash(into hasher: inout Hasher) {
-        hasher.combine(position.x)
-        hasher.combine(position.y)
-        hasher.combine(index)
-    }
-}
-
-extension Optional where Wrapped == Direction {
-    var description: String {
-        switch self {
-        case .up?:
-            return "move up"
-        case .down?:
-            return "move down"
-        case .left?:
-            return "move left"
-        case .right?:
-            return "move right"
-        case nil:
-            return "wait"
-        default:
-            fatalError()
-        }
+        hasher.combine(position)
+        hasher.combine(round)
     }
 }
--- Tests/Day24Tests/Day24Tests.swift
+++ Tests/Day24Tests/Day24Tests.swift
@@ -16,12 +16,27 @@ final class Day24Tests: XCTestCase {
         XCTAssertEqual(answer, 18)
     }
 
+    func testPart2Example() {
+        let input = """
+        #.######
+        #>>.<^<#
+        #.<..<<#
+        #>v.><>#
+        #<^v^^>#
+        ######.#
+        """
+
+        let answer = findWayThroughBlizzardAndBack(input)
+        XCTAssertEqual(answer, 54)
+    }
+
     func testDay24Part1() { 
         let answer = findWayThroughBlizzard(input)
         print("Day 24 Part 1:", answer)
     }
 
     func testDay24Part2() {
-        print("Day 24 Part 2:")
+        let answer = findWayThroughBlizzardAndBack(input)
+        print("Day 24 Part 2:", answer)
     }
 }
